99b5fc527d054ecfd8c95703161e84fba2399812
Lukas <lsuchel@live.com>
2025-04-29 00:37:13 -0700

    Let's get this going

---
+++ testing/src/main.js
@@ -0,0 +1,44 @@
+import './style.css'
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render( Scene, camera);
+
+const geometry = new THREE.TorusGeometry(10,3,16,100)
+const material = new THREE.MeshBasicMaterial( {color:rgb(204, 213, 255), wireframe: true});
+const torus = new THREE.Mesh(geometry, material);
+
+Scene.add(torus)
+
+const pointLight = new THREE.pointLight(oxffffff)
+pointLight.position.set(5,5,5)
+
+const ambientLight = new THREE.AmbientLight(oxffffff);
+scene.add(pointLight,ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+
+function animate(){
+  requestAnimationFrame( animate);
+
+  torus.rotation.x +=0.01;
+  torus.rotation.y +=0.005;
+  torus.rotation.z +=0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate()
