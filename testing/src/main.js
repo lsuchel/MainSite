5349eeb89d3a4e1c9c9b7edbbf2624f69068ac01
Lukas <lsuchel@live.com>
2025-06-19 22:41:16 -0700

    Prod Push with main updates before testing

--- testing/src/main.js
+++ testing/src/main.js
@@ -35,8 +35,8 @@ scene.add(lightHelper, gridHelper)
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar(){
-  const geometry =  new THREE.SphereGeometry(0.15,24,24);
-  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const geometry =  new THREE.SphereGeometry(0.3,24,24);
+  const material = new THREE.MeshStandardMaterial({color: 0xebfc53})
   const star = new THREE.Mesh(geometry, material);
 
   const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
@@ -50,7 +50,45 @@ Array(115).fill().forEach(addStar);
 const bgLoader = new THREE.TextureLoader().load('./Images/space.jpg');
 scene.background = bgLoader;
 
+const Texture = new THREE.TextureLoader().load('./Images/hand.jpg');
 
+const T = new THREE.Mesh(
+  new THREE.BoxGeometry(2.7,2.7,2.7),
+  new THREE.MeshBasicMaterial({ map:Texture })
+);
+
+scene.add(T);
+
+const MoonTexture = new THREE.TextureLoader().load('./Images/Moon.jpg');
+
+const MT = new THREE.Mesh(
+  new THREE.SphereGeometry(3,32,32),
+  new THREE.MeshStandardMaterial({ map:MoonTexture, normalMap:MoonTexture })
+);
+
+scene.add(MT);
+
+MT.position.z = 15;
+MT.position.setX(-10);
+
+
+function moveCamera(){
+  
+  const t = document.body.getBoundingClientRect().top;
+  MT.rotation.x += 0.05;
+  MT.rotation.y += 0.075;
+  MT.rotation.z += 0.05;
+
+  T.rotation.y += 0.02;
+  T.rotation.z += 0.02;
+
+  camera.position.x = t * -0.002;
+  camera.position.y= t * -0.002;
+  camera.position.z = t * -0.01;
+
+}
+
+document.body.onscroll = moveCamera
 
 function animate(){
   requestAnimationFrame(animate);
